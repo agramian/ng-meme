1c8d54c632645b1a53e7bf9c4e538c86c2e1cf5c
Abtin Gramian <abtin.gramian@gmail.com>
2016-03-29 00:36:07 -0700

    added skeleton code for meme directive handling, added button to eventually open raw image in new window (currently just triggers alert)

--- demo/controllers.js
+++ demo/controllers.js
@@ -1,2 +1,33 @@
 'use strict';
-var ngMemeDemoAppControllers = angular.module('ngMemeDemoAppControllers', []);
+angular.module('ngMemeDemoApp').controller('ngMemeDemoAppCtrl', function($scope //, MemeAPI
+) {
+  /*
+  $scope.memeImageHeight = 0;
+	$scope.memeImageWidth = 0;
+	$scope.memeLoadedCallback = function() {
+		sharedProperties.setLoadingImage(false);
+	};
+	$scope.memeLoadedErrback = function() {
+		$scope.shareImage = 'img/error.svg';
+	};
+	$scope.updateMemeCanvas = function() {
+		$scope.memeImageHeight = document.getElementById('memeDiv').offsetHeight;
+		$scope.memeImageWidth = document.getElementById('memeDiv').offsetWidth;
+	};
+  */
+	$scope.memeTopLinePlaceholder = "top line (optional)";
+	$scope.memeBottomLinePlaceholder = "bottom line (optional)";
+  /*
+	$scope.memeTopLineText = "";
+	$scope.memeBottomLineText = "";
+	$scope.validateMemeText = function () {
+		$scope.memeTopLineText = $scope.memeTopLineText.slice(0, $scope.memeInputMaxLength);
+		$scope.memeBottomLineText = $scope.memeBottomLineText.slice(0, $scope.memeInputMaxLength);
+	};
+	$scope.memeInputMaxLength = 100;
+	$scope.memeApi = new MemeAPI();
+  */
+  $scope.openRawImageInNewWindow = function() {
+    alert("opening in new window!");
+  }
+});
